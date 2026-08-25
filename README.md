d88d3fdfe9cf32a330e3eb9905883242623c90c0
John Arundel <john@bitfieldconsulting.com>
2026-08-25 10:18:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to the machine
+
+![Gophers floating in space](img/space-gophers.webp)
+
+This repo contains suggested solutions to the challenges in the R8 emulator project, described here:
+
+* [Welcome to the machine: emulating a CPU](https://bitfieldconsulting.com/posts/welcome-to-machine)
---
+++ r8_2/src/lib.rs
@@ -0,0 +1,53 @@
+pub const NOP: u8 = 1;
+
+#[derive(Debug)]
+pub struct Cpu {
+    pub pc: u16,
+    pub mem: [u8; 65536],
+}
+
+impl Default for Cpu {
+    fn default() -> Self {
+        Self {
+            pc: 0,
+            mem: [0; 65536],
+        }
+    }
+}
+
+impl Cpu {
+    pub fn step(&mut self) {
+        let instruction = self.mem.get(usize::from(self.pc)).unwrap_or(&0);
+        self.pc = self.pc.wrapping_add(1);
+        match *instruction {
+            NOP => {
+                // nothing to do
+            }
+            other => unimplemented!("opcode {other}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_correctly_initialises_cpu() {
+        let cpu = Cpu::default();
+        assert_eq!(cpu.pc, 0, "wrong initial PC");
+        assert_eq!(*cpu.mem.first().unwrap(), 0, "wrong memory contents");
+    }
+
+    #[test]
+    fn step_increments_pc() {
+        let mut cpu = Cpu::default();
+        cpu.mem[256] = NOP;
+        cpu.mem[257] = NOP;
+        cpu.pc = 256;
+        cpu.step();
+        assert_eq!(cpu.pc, 257, "wrong PC after first step()");
+        cpu.step();
+        assert_eq!(cpu.pc, 258, "wrong PC after second step()");
+    }
+}
